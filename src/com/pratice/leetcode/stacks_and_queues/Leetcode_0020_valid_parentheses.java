a8dc014973186d2c4bfe16e06d41684d2bbdaefe
Loren-Cheng <73529790+Loren-Cheng@users.noreply.github.com>
2023-02-03 17:33:22 +0800

    update

---
+++ src/com/pratice/leetcode/stacks_and_queues/Leetcode_0020_valid_parentheses.java
@@ -0,0 +1,39 @@
+package com.pratice.leetcode.stacks_and_queues;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class Leetcode_0020_valid_parentheses {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        Map<Character, Character> matching = new HashMap<>();
+        matching.put('(', ')');
+        matching.put('{', '}');
+        matching.put('[', ']');
+
+        for (int i = 0; i < s.length(); i++) {
+            if (matching.containsKey(s.charAt(i))) {
+                stack.push(s.charAt(i));
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+                if (matching.get(stack.peek()) == s.charAt(i)) {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return stack.empty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+        System.out.println(isValid("()[]{}"));
+        System.out.println(isValid("(]"));
+        System.out.println(isValid("]"));
+        System.out.println(isValid("(){}}{"));
+    }
+}

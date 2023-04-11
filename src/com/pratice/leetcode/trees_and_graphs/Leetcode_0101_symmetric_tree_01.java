0a9d4888f45ebf31af347896d3cbbf816aca2140
Loren-Cheng <73529790+Loren-Cheng@users.noreply.github.com>
2023-04-11 14:01:31 +0800

    update

---
+++ src/com/pratice/leetcode/trees_and_graphs/Leetcode_0101_symmetric_tree_01.java
@@ -0,0 +1,17 @@
+package com.pratice.leetcode.trees_and_graphs;
+
+import java.util.*;
+
+public class Leetcode_0101_symmetric_tree_01 {
+    public boolean isSymmetric(TreeNode root) {
+        return dfs(root.left, root.right);
+    }
+
+    boolean dfs(TreeNode leftNode, TreeNode rightNode) {
+        if (leftNode == null && rightNode == null) return true;
+        if (leftNode == null || rightNode == null) return false;
+        return (leftNode.val == rightNode.val)
+                && dfs(leftNode.left, rightNode.right)
+                && dfs(leftNode.right, rightNode.left);
+    }
+}
